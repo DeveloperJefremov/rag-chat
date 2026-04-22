6cadcc3973f590bfd751298592063e48adcf2d7a
Artjoms Jefremovs <developerjefremov@gmail.com>
2026-04-22 21:09:44 +0300

    feat: update package.json with new scripts and dependencies, add Prisma configuration
    feat: create a reusable Button component with variants and styles
    
    feat: add Prisma configuration files and schema for PostgreSQL
    
    chore: add utility functions for class name management
    
    chore: remove deprecated globals.css file
    
    chore: add test and server directories
    
    chore: update tsconfig.json for improved path resolution and exclusions
    
    chore: add Vitest configuration for testing setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
+	prettier,
+	{
+		rules: {
+			'no-console': ['warn'],
+			// 'no-unused-vars': ['warn'],
+			'no-inner-declarations': ['error', 'functions'],
+			'prefer-arrow-callback': 'error',
+			'prefer-template': ['warn'],
+		},
+	},
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		'.next/**',
+		'out/**',
+		'build/**',
+		'next-env.d.ts',
+	]),
 ]);
 
 export default eslintConfig;
